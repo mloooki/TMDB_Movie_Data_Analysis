f1726e4ae8f1f69daf8c067c14c2d5f171c937a9
MLoooKi <mloooki.3@gmail.com>
2020-06-08 18:02:29 +0300

    Create new files

---
+++ TMDB_Movie_Data_Analysis.py
@@ -0,0 +1,21 @@
+#import the libraries we need in this project.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df= pd.read_csv("tmdb-movies.csv",parse_dates=['release_date']) #load dataset
+df.head() # see first 5 rows from the dataset.
+
+df.shape #to get number of records and columns
+
+df.info() 
+
+df.drop(['id' , 'imdb_id' , 'original_title' , 'homepage', 'tagline' ,'director' , 'cast' ,'overview','production_companies','release_year','keywords','genres','budget_adj','revenue_adj'] ,axis=1, inplace=True)
+
+df.head() # to see the data after drop.
+
+df.hist(figsize=(10,8)); # histogram for all columns.
+
+df.info()
+
+df.describe()
